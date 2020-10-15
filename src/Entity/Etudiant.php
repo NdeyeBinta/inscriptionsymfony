a545d146363cde0f7cc546412f2675e87fe5968c
Ndeye binta Diene <ndeyebintadiene@gmail.com>
2020-10-15 17:09:00 +0000

    'inscriptionsymfony'

---
+++ src/Entity/Etudiant.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\EtudiantRepository")
+ */
+class Etudiant
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $mat;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $nom;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $prenom;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getMat()
+    {
+        return $this->mat;
+    }
+
+    public function setMat(int $mat)
+    {
+        $this->mat = $mat;
+
+        return $this;
+    }
+
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom)
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getPrenom()
+    {
+        return $this->prenom;
+    }
+
+    public function setPrenom(string $prenom)
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+}
---
+++ src/Entity/Inscription.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\InscriptionRepository")
+ */
+class Inscription
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $lib;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $date;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getLib()
+    {
+        return $this->lib;
+    }
+
+    public function setLib(string $lib)
+    {
+        $this->lib = $lib;
+
+        return $this;
+    }
+
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTimeInterface $date)
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+}
